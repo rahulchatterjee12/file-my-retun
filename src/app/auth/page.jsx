93bd2fa8bb6b422faac43c63eb2f1a7b6284cd00
rc5292684 <rc5292684@gmail.com>
2024-12-13 17:44:37 +0530

    fix register and login page

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -28,22 +28,36 @@ const Login = () => {
   return (
     <div className="bg-white bg-right-blue">
       {/* <Header /> */}
-      <div className="max-w-[1140px] 2xl:max-w-[1320px] px-3 lg:px-10 xl:px-0 mx-auto my-[100px]">
-        <div className="md:flex justify-between items-center">
+      <div className="max-w-[1140px] 2xl:max-w-[1320px] lg:px-10 xl:px-0 mx-auto mb-[100px]">
+        <div className="md:flex justify-between items-start">
           <div
-            className="w-full md:w-[40%] lg:w-[62%] mt-6"
+            className="w-full md:w-[40%] lg:w-[62%]"
             data-aos="fade-left"
             data-aos-delay="900"
           >
-            <Image
-              src="/assets/images/png/loginRegister.png"
-              alt="logo"
-              width={835}
-              height={557}
-            />
+            <div className="hidden md:block">
+              <Image
+                src="/assets/images/register.png"
+                alt="logo"
+                width={774}
+                height={774}
+              />
+            </div>
+            <div className="block md:hidden">
+              <Image
+                src="/assets/images/register-phone.png"
+                alt="logo"
+                width={774}
+                height={774}
+              />
+            </div>
           </div>
 
-          <div className='md:w-[35%] lg:w-[25%] w-full" data-aos="fa de-right" data-aos-delay="800'>
+          <div
+            className="md:w-[35%] lg:w-[25%] w-full px-3 mt-8"
+            data-aos="fa de-right"
+            data-aos-delay="800"
+          >
             <h1 className="text-black text-[30px] font-semibold pb-5">
               Welcome back 👋
             </h1>
