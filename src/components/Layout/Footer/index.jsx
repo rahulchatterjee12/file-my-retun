35809010b37d21933ee3b203b710ab17eb743248
rc5292684 <rc5292684@gmail.com>
2024-12-21 17:49:03 +0530

    working on footer

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="flex">Footer</div>;
+  return (
+    <div className="px-[145px] py-[70px] bg-[#293854] text-white">
+      <div>
+        <h3 className="text-[24px]">FILEMYRETURN</h3>
+        <p className="mt-[24px]">
+          404, 4th Floor, <br />
+          DLF Galleria Towers, DLF Phase IV, <br />
+          Gurgaon-122009, Haryana
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
